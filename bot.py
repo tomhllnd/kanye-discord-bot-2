33765bcd9e292040293937ea1e6136eca2f95fcb
tomhllnd <46725304+tomhllnd@users.noreply.github.com>
2019-01-16 18:13:45 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -77,7 +77,7 @@ async def on_message(message):
 @client.event
 async def on_member_join(member):
     embed = discord.Embed(title="pluggduk Patreon", url='http://patreon.com/pluggduk', color=0x2fe14e)
-    embed.set_author(name="Welcome to pluggdUK.", icon_url="https://cdn.discordapp.com/attachments/533238248781250563/535116197591646218/help.png")
+    embed.set_author(name="Welcome to pluggdUK.")
     embed.add_field(name='Welcome', value='We are a UK-based cook group, who look forward to helping you make profit. We charge a small fee of £15 per month, which relative to other cook groups, is very small.\n ', inline=False)
     embed.add_field(name='Features', value='We offer 24/7 advice, groupbuys (recently offered members chance to buy NSB for $425, now selling at $1k), **Free** Supreme slots and **very** cheap Shopify slots on bots like Cyber, Instant homemade restock monitors, REsell Predicitons, accurate early links, and much more. ', inline=False)
     embed.add_field(name='Become a Member', value='In order to become a paying member, you need to click on the link to our patreon above, and become the Chef tier. After that, you need to link your discord with patreon, and you will recieve your role.', inline=False)
