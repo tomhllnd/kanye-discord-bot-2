1522f3e09660f88d3e34b636f9267a55d5fca932
tomhllnd <46725304+tomhllnd@users.noreply.github.com>
2019-01-16 18:06:02 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,11 +68,21 @@ async def on_message(message):
     if message.content.startswith("kanye, help"):
         embed = discord.Embed(color=0x2fe14e)
         embed.set_author(name="Hey, I'm Kanye.", icon_url="https://cdn.discordapp.com/attachments/533238248781250563/535116197591646218/help.png")
-        embed.add_field(name='Fee Calculator -', value='Beckon me with `kanye, fees <amount>` and I will calculate the fees each of the sites you dirty resellers use will take', inline=False)
-        embed.add_field(name='Facts and Quotes -', value='Beckon me with `kanye, fact` and I will recite one of my quotes or stories', inline=False)
-        embed.add_field(name='Ping -', value='Check if im running and online with `kanye, ping`. If you spam it, Ill get hella triggered.', inline=False)
+        embed.add_field(name='Fee Calculator', value='Beckon me with `kanye, fees <amount>` and I will calculate the fees each of the sites you dirty resellers will take', inline=False)
+        embed.add_field(name='Facts and Quotes', value='Beckon me with `kanye, fact` and I will recite one of my quotes or stories', inline=False)
+        embed.add_field(name='Ping', value='Check if im running and online with `kanye, ping`. If you spam it, Ill get hella triggered ( ͡☉ ͜ʖ ͡☉)', inline=False)
         embed.set_footer(text="Kanye Bot made by Plug#5464")
         await client.send_message(message.channel,embed=embed)
+
+@client.event
+async def on_member_join(member):
+    embed = discord.Embed(title="pluggduk Patreon", url='http://patreon.com/pluggduk', color=0x2fe14e)
+    embed.set_author(name="Welcome to pluggdUK.", icon_url="https://cdn.discordapp.com/attachments/533238248781250563/535116197591646218/help.png")
+    embed.add_field(name='Welcome', value='We are a UK-based cook group, who look forward to helping you make profit. We charge a small fee of £15 per month, which relative to other cook groups, is very small.\n ', inline=False)
+    embed.add_field(name='Features', value='We offer 24/7 advice, groupbuys (recently offered members chance to buy NSB for $425, now selling at $1k), **Free** Supreme slots and **very** cheap Shopify slots on bots like Cyber, Instant homemade restock monitors, REsell Predicitons, accurate early links, and much more. ', inline=False)
+    embed.add_field(name='Become a Member', value='In order to become a paying member, you need to click on the link to our patreon above, and become the Chef tier. After that, you need to link your discord with patreon, and you will recieve your role.', inline=False)
+    embed.set_footer(text="Kanye Bot made by Plug#5464")
+    await client.send_message(member, embed=embed)
         
 
 
